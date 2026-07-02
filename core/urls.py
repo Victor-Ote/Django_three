d8e17bb2b43e9e3eefc11973cfaf4a714e3f4667
Dental Market <victorotelakoski@gmail.com>
2026-07-02 14:43:35 -0300

    URLS

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, ProductListView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('', ProductListView.as_view(), name='product'),
 ]
