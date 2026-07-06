88beb95b52d87aa039c750ae690c64e7857fe78b
Victor_Ote <victorotelakoski@gmail.com>
2026-07-06 17:14:18 -0300

    Product R

--- product/views.py
+++ product/views.py
@@ -5,10 +5,18 @@ from django.views.generic import TemplateView, ListView
 
 class ProductListView(ListView):
     model = Product
-    queryset = Product.objects.all()
+    context_object_name = "products"
     template_name = 'list.html'
 
     def dispatch(self, request, *args, **kwargs):
 
         print('Caiu para o App Product e está na view')
-        return super().dispatch(request, *args, **kwargs) 
+        return super().dispatch(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["title"] = "Produtos"
+        context["total"] = Product.objects.count()
+
+        return context
