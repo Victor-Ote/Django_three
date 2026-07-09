242aba59a439b39d0dd769f44bc7719275eed8f9
Dental Market <victorotelakoski@gmail.com>
2026-07-09 15:05:55 -0300

    Client funcionando

--- client/models.py
+++ client/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from core.models import Base
 
 class Client(Base, models.Model):
-    name = models.CharField('name', max_length=20)
+    name = models.CharField('name', max_length=20, unique=True)
+    doc_type = models.CharField('doc_type', max_length=20, choices=[('PF', 'Pessoa Física'), ('PJ', 'Pessoa Jurídica')])
+    doc_number = models.CharField('doc_number', max_length=20, unique=True)
 
     def __str__(self):
         return self.name
